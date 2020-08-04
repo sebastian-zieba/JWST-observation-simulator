7c12d0267f55864f5ff1e67bdc72448f0d3a5ce0
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2020-08-04 13:37:17 +0200

    added comments in run.py, incl. transmission in plot

--- run.py
+++ run.py
@@ -14,6 +14,8 @@ from shutil import copyfile
 import yaml
 
 ### BINNING FUNCTION ###
+# Note that the binning is constant
+# One would have to modify that function if the binsize should be a function of wavelength
 def bins(x, y, y_err, n_bins):
     indexes = np.array_split(np.arange(len(x)), n_bins)
     #print((indexes[0]).size)
@@ -23,11 +25,12 @@ def bins(x, y, y_err, n_bins):
     return (binned_x, binned_y, binned_y_err)
 
 
+### IMPORT CONFIG FILE ###
 with open('./config/params.yaml', 'r') as file:
     params = yaml.safe_load(file)
 
-for k,v in params.items():
-    print(k, type(v))
+#for k,v in params.items():
+#    print(k, type(v))
 
 class AncillaryData:
     """
@@ -59,7 +62,7 @@ ancil = AncillaryData(params)
 ### SAVE OUTPUT IF WISHED ###
 
 if ancil.output == True:
-    dirname = "runs_dir/" + datetime.strftime(datetime.now(), '%m_%d_%H_%M')
+    dirname = "runs_dir/" + datetime.strftime(datetime.now(), '%b_%d_%H:%M:%S')
     if not os.path.exists(dirname): os.makedirs(dirname)
 
     resultfile = open(dirname+'/results.txt', 'w')
@@ -83,12 +86,11 @@ for i in pce_tp:
     else:
         mask_pce.append(False)
 
-wvl_range = [wvl_tp[mask_pce][0], wvl_tp[mask_pce][-1]]
+wvl_range = [wvl_tp[mask_pce][0], wvl_tp[mask_pce][-1]] #range in which instrument operates above 10%max
 
-print(wvl_range)
+print('Wavelength range of the instrument: ', ['{:.3f}'.format(num) for num in wvl_range])
 
 
-### ACTUAL RELEVANT PART CONTINUES
 
 ### RUN PANDEXO
 
@@ -122,6 +124,8 @@ exo_dict['star']['logg'] = ancil.logg             #log surface gravity cgs
 
 ### EXOPLANET INPUTS ###
 
+### IF NO MODEL, USE CONSTANT
+
 if ancil.path_to_model == False:
     exo_dict['planet']['type'] = 'constant'                  #tells pandexo you want a fixed transit depth
     exo_dict['planet']['transit_duration'] = ancil.D*60.0*60.0   #transit duration
@@ -137,6 +141,8 @@ if ancil.path_to_model == False:
     #exo_dict['planet']['f_unit'] = 'fp/f*'
     #exo_dict['planet']['temp'] = T_day(Ts, ars, 0)[0]
 
+### IF PATH HAS MODEL, USE IT
+
 else:
     exo_dict['planet']['type'] = 'user'  # tells pandexo you are uploading your own spectrum
     exo_dict['planet']['exopath'] = ancil.path_to_model
@@ -146,15 +152,17 @@ else:
     exo_dict['planet']['td_unit'] = 's'  # Any unit of time in accordance with astropy.units can be added
 
 
+### RUN IT ###
 
 result = jdi.run_pandexo(exo_dict,[ancil.instrument])
 
 
+# ONLY USE THE PARTS WHERE THE INSTUMENT OPERATES WELL
 mask = [wvl_range[0] < wvli < wvl_range[1] for wvli in result['FinalSpectrum']['wave']]
 
-fig, ax = plt.subplots(1,1,figsize=(11,5))
-#ax.errorbar(result['FinalSpectrum']['wave'][mask], result['FinalSpectrum']['spectrum'][mask], yerr=result['FinalSpectrum']['error_w_floor'][mask], fmt='.', ls='')
 
+# PLOT IT
+fig, ax = plt.subplots(1,1,figsize=(11,5))
 
 ax.axvline(wvl_range[0], c='r', ls='--')
 ax.axvline(wvl_range[1], c='r', ls='--')
@@ -163,27 +171,34 @@ ax.set_xlabel('wavelength (microns)')
 ax.set_ylabel('tansit depth (rprs**2)')
 
 
-
-### PLOT THROUGHPUT
-#ax.plot(wvl_tp , (pce_tp+800e-6)/max((pce_tp+800e-6)/1200e-6), label='throughput (arb. unit)')
-
-
 ### CALC NUMBER OF BINS ###
 
 n_bins = (wvl_range[1]-wvl_range[0])/(wvl_range[1]+wvl_range[0]) * 2 * ancil.R
-n_bins = round(n_bins)
-print('n_bins = ', n_bins)
-print('R = ', n_bins/2 * (wvl_range[1]+wvl_range[0])/(wvl_range[1]-wvl_range[0]))
+n_bins = int(round(n_bins)) #rounding will lead to a resolution which is a bit off the wanted value (could be improved)
+print('Number of bins = ', n_bins)
+print('Final resolution = ', '{:.3f}'.format( n_bins/2 * (wvl_range[1]+wvl_range[0])/(wvl_range[1]-wvl_range[0]) ))
 
-a,b,c=bins(result['FinalSpectrum']['wave'][mask], result['FinalSpectrum']['spectrum'][mask], result['FinalSpectrum']['error_w_floor'][mask], 13)
+a,b,c=bins(result['FinalSpectrum']['wave'][mask], result['FinalSpectrum']['spectrum'][mask], result['FinalSpectrum']['error_w_floor'][mask], n_bins)
 
 ax.errorbar(a, b, yerr=c, fmt='.', ls='', label='spectrum')
 
-print('hier: ', np.median(c))
+print('median error bar size: ', '{:.3e}'.format( np.median(c)) )
 
 
-ax.set_xlim(3.8, 5.05)
-ax.set_ylim(800e-6, 1200e-6)
+# PLOT TRANSMISSION AND MAKE LIMITS GREAT
+
+xlim0, xlim1 = ax.get_xlim()
+
+ylim0, ylim1 = ax.get_ylim()
+ylim_range = ylim1 - ylim0
+ylim_scaler=0.4
+ax.set_ylim(ylim0 - ylim_scaler * ylim_range, ylim1 + ylim_scaler * ylim_range)
+
+ylim0, ylim1 = ax.get_ylim()
+pce_tp_rescale = pce_tp * (ylim1-ylim0)/(max(pce_tp)*3-0) + ylim0
+ax.plot(wvl_tp, pce_tp_rescale, ls='-.', label='bandpass')
+
+ax.set_xlim(xlim0,xlim1)
 
 plt.title('{0}'.format(ancil.instrument))
 
@@ -199,11 +214,12 @@ np.savetxt(dirname + "/spectrum.txt", list(zip(a,b,c)))
 n_lam = 2/(exo_dict['planet']['transit_duration']*result['FinalSpectrum']['error_w_floor']**2)
 
 # in 1 sec
-print('one sec:', np.sqrt(sum(n_lam))/(sum(n_lam))*1e6)
+print('RMS one sec:', '{:.3f}'.format(np.sqrt(sum(n_lam))/(sum(n_lam))*1e6))
 # in 1 min #"it only takes a minute girl"
-print('one min:', np.sqrt(sum(n_lam)*60)/(sum(n_lam)*60)*1e6)
+print('RMS one min:', '{:.3f}'.format(np.sqrt(sum(n_lam)*60)/(sum(n_lam)*60)*1e6))
 # in 30 minute
-print('30 mins:', np.sqrt(sum(n_lam)*60*30)/(sum(n_lam)*60*30)*1e6)
+print('RMS 30 mins:', '{:.3f}'.format(np.sqrt(sum(n_lam)*60*30)/(sum(n_lam)*60*30)*1e6))
+
 
 if ancil.output == True:
     for k, v in result['timing'].items():
@@ -216,9 +232,16 @@ if ancil.output == True:
 
     print('\n', file=resultfile)
 
-    print('rms one sec:', np.sqrt(sum(n_lam)) / (sum(n_lam)) * 1e6, file=resultfile)
-    print('rms one min:', np.sqrt(sum(n_lam) * 60) / (sum(n_lam) * 60) * 1e6, file=resultfile)
-    print('rms 30 mins:', np.sqrt(sum(n_lam) * 60 * 30) / (sum(n_lam) * 60 * 30) * 1e6, file=resultfile)
+    print('Number of bins = ', n_bins, file=resultfile)
+    print('Final resolution = ',
+          '{:.3f}'.format(n_bins / 2 * (wvl_range[1] + wvl_range[0]) / (wvl_range[1] - wvl_range[0])), file=resultfile)
+    print('median error bar size: ', '{:.3e}'.format(np.median(c)), file=resultfile)
+
+    print('\n', file=resultfile)
+
+    print('rms one sec:', '{:.3f}'.format(np.sqrt(sum(n_lam)) / (sum(n_lam)) * 1e6), file=resultfile)
+    print('rms one min:', '{:.3f}'.format(np.sqrt(sum(n_lam) * 60) / (sum(n_lam) * 60) * 1e6), file=resultfile)
+    print('rms 30 mins:', '{:.3f}'.format(np.sqrt(sum(n_lam) * 60 * 30) / (sum(n_lam) * 60 * 30) * 1e6), file=resultfile)
 
 if ancil.output == True:
     resultfile.close()
